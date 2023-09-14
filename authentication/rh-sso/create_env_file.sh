196609901d9a3beb7ea339b22db49f86d467f50f
6871 <55576043+6871@users.noreply.github.com>
2023-09-14 21:38:54 +0100

    Use .env file for compose.yaml credential values; add script to generate .env file

---
+++ authentication/rh-sso/create_env_file.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Generate environment variable values for compose.yaml file; to use:
+# ./create_env_file.sh > .env
+printf 'RH_SSO_DB_USER=rh-sso-db-user\n'
+printf 'RH_SSO_DB_USER_PASSWORD=%s\n' "$(openssl rand -base64 48)"
+printf 'RH_SSO_KEYSTORE_PASSWORD=%s\n' "$(openssl rand -base64 48)"
+printf 'RH_SSO_ADMIN_USER=admin\n'
+printf 'RH_SSO_ADMIN_USER_PASSWORD=%s\n' "$(openssl rand -base64 48)"
