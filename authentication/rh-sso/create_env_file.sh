36644950db4130a365998dba40cf69e08a275042
6871 <55576043+6871@users.noreply.github.com>
2023-09-17 11:02:55 +0100

    Remove KEYSTORE_PASSWORD env var

--- authentication/rh-sso/create_env_file.sh
+++ authentication/rh-sso/create_env_file.sh
@@ -3,6 +3,5 @@
 # ./create_env_file.sh > .env
 printf 'RH_SSO_DB_USER=rh-sso-db-user\n'
 printf 'RH_SSO_DB_USER_PASSWORD=%s\n' "$(openssl rand -base64 48)"
-printf 'RH_SSO_KEYSTORE_PASSWORD=%s\n' "$(openssl rand -base64 48)"
 printf 'RH_SSO_ADMIN_USER=admin\n'
 printf 'RH_SSO_ADMIN_USER_PASSWORD=%s\n' "$(openssl rand -base64 48)"
